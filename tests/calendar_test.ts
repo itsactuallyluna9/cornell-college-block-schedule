60a4bc0d4ab126e006eda90302eef5957ecb2e35
Mark Smith <totallynotmark6@gmail.com>
2023-08-01 19:17:23 -0500

    fix tests
    fixes #1

--- tests/calendar_test.ts
+++ tests/calendar_test.ts
@@ -1,29 +1,38 @@
 import {
   assertEquals,
   assertExists,
+  assertInstanceOf,
+  assertNotEquals,
 } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 import loadCalendar from "../src/calendar.ts";
 
-Deno.test("calendar data is loaded", () => {
+Deno.test("calendar data can load", () => {
   const calendar = loadCalendar();
   assertExists(calendar);
 });
 
 Deno.test("calendar data is valid", () => {
   const calendar = loadCalendar();
+  const invalidDate = new Date(null!);
   for (const year in calendar) {
     const year_data = calendar[year];
-    assertEquals(year_data.blocks.length > 8, true);
+    assertEquals(year_data.blocks.length >= 8, true);
     for (const block of year_data.blocks) {
       assertEquals(typeof block.block, "number");
       assertEquals(block.block >= 1, true);
 
-      assertEquals(block.start instanceof Date, true);
-      assertEquals(block.end instanceof Date, true);
+      assertInstanceOf(block.start, Date);
+      assertInstanceOf(block.end, Date);
+
+      assertNotEquals(block.start, invalidDate);
+      assertNotEquals(block.end, invalidDate);
     }
     for (const event of year_data.events) {
-      assertEquals(event.start instanceof Date, true);
-      assertEquals(event.end instanceof Date, true);
+      assertInstanceOf(event.start, Date);
+      assertInstanceOf(event.end, Date);
+
+      assertNotEquals(event.start, invalidDate);
+      assertNotEquals(event.end, invalidDate);
     }
   }
 });
